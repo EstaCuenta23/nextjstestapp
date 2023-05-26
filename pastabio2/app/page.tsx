e52e37e13acb1a7a7b387596156273d41f29a466
EstaCuenta23 <134388512+EstaCuenta23@users.noreply.github.com>
2023-05-26 12:08:19 +0000

    First commit

---
+++ pastabio2/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <h1>
+        Hola
+      </h1>
+      <p>
+        Esta es mi pagina web
+      </p>
+    </>
+    
+  )
+}
